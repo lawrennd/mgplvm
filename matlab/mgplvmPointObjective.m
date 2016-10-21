7c460ca647850e2d217a0121e92b01a91e8712b9
Neil Lawrence <N.Lawrence@sheffield.ac.uk>
2016-10-21 14:53:12 +0100

    Create mgplvmPointObjective.m
    Still need mgplvmPointLogLikelihood.m

---
+++ matlab/mgplvmPointObjective.m
@@ -0,0 +1,21 @@
+function f = mgplvmPointObjective(x, model, y)
+
+% MGPLVMPOINTOBJECTIVE Wrapper function for objective of a single point in latent space and the output location..
+% FORMAT
+% DESC provides a wrapper function for the negative log probability
+% of a given data point under the posterior distribution of the
+% Gaussian process induced by the training data.
+% ARG x : location in input space for the point.
+% ARG model : the model structure for which the negative log
+% probability of the given data under the posterior is to be computed.
+% ARG y : the location in data space for the point.
+% RETURN f : the negative of the log probability of the given data
+% point under the posterior distribution induced by the training data.
+% 
+% SEEALSO : mgplvmCreate, mgplvmPointLogLikelihood, mgplvmOptimisePoint
+%
+% COPYRIGHT : Neil D. Lawrence, 2006, 2016
+
+% FGPLVM
+
+f = - mgplvmPointLogLikelihood(model, x, y);
